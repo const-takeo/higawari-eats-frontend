d57e6d5633416bd48e004d79010fa82cf6244e5c
const-takeo <const.takeo@gmail.com>
2021-01-24 00:30:08 +0900

    Apollo Client Setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './apollo';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
