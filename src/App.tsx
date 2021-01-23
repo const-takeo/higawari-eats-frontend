dfbcd9410dbe8a2e1adda7638b66154dbef893f3
const-takeo <const.takeo@gmail.com>
2021-01-24 00:40:20 +0900

    All the FrontEnd Setup Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import LoggedOutRouter from './routers/logged-out-router';
 import "./styles/style.css";
 
 function App() {
   return (
-    <div className=" bg-black hover:bg-transparent">
-      <h1 className=" text-xl text-white ">Hello TailWind</h1>
+    <div>
+      <LoggedOutRouter />
     </div>
   );
 }
---
+++ src/routers/logged-out-router.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const LoggedOutRouter = () => <span>LoggedOut</span>;
+
+export default LoggedOutRouter
