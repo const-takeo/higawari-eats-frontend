8a7fcdd3e800c339e007362bf4e76770d1a611a8
const-takeo <const.takeo@gmail.com>
2021-01-24 22:36:10 +0900

    Login Form Design is Done

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,22 +1,59 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+interface ILoginForm {
+  email?: string;
+  password?: string;
+}
 
 const Login = () => {
+  const { register, errors, getValues, handleSubmit } = useForm<ILoginForm>();
+  const onSubmit = () => {
+    console.log(getValues());
+  };
   return (
     <div className="flex bg-gradient-to-t from-purple-800 to-blue-100 h-screen items-center justify-center">
-      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
+      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
         <h3 className=" text-xl text-gray-500">ログイン</h3>
-        <form className="flex flex-col mt-5 px-5">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid gap-3 mt-5 px-5"
+        >
           <input
+            ref={register({ required: "メールを入力して下さい。" })}
+            required
+            type="email"
+            name="email"
             placeholder="メール"
-            className=" shadow-inner bg-gray-100 border-2 focus:outline-none focus:border-purple-300 rounded-lg py-3 mb-2 px-5"
+            className="inputCss mb-2"
           />
+          {errors.email?.message && (
+            <span className="font-medium text-red-500">
+              {errors.email.message}
+            </span>
+          )}
           <input
+            ref={register({
+              required: "パスワードを入力して下さい。",
+              minLength: 10,
+            })}
+            required
+            type="password"
+            name="password"
             placeholder="パスワード"
-            className=" shadow-inner bg-gray-100 border-2 focus:outline-none focus:border-purple-300 rounded-lg py-3 px-5"
+            className="inputCss"
           />
-          <button className="py-3 px-5 mt-2 bg-purple-600 text-white hover:opacity-90 focus:outline-none rounded-lg text-lg">
-            ログイン
-          </button>
+          {errors.password?.type === "minLength" && (
+            <span className="font-medium text-red-500">
+              パスワードを10文字以上入力して下さい。
+            </span>
+          )}
+          {errors.password?.message && (
+            <span className="font-medium text-red-500">
+              {errors.password.message}
+            </span>
+          )}
+          <button className="mt-2 btnCss">ログイン</button>
         </form>
       </div>
     </div>
