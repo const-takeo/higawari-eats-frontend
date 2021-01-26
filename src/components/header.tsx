2068b5a873e495c94e530256c373a52b74624b98
const-takeo <const.takeo@gmail.com>
2021-01-26 12:40:52 +0900

    Header & Apollo Cache

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,33 @@
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
+import { useMe } from "../hooks/useMe";
 import higawariLogo from "../images/logo.svg";
 //emは一番近いparentを基準として決める。
 //remはrootつまりbodyのfont-sizeを基準として決める。
 //bodyのfont-sizeの探し方はinspect-> compute -> font-sizeを検索。
-export const Header = () => (
-  <header className="py-4">
-    <div className="mx-auto w-screen max-w-screen-2xl">
-      <img src={higawariLogo} className=" w-36 mb-5" alt="Higawari-eats-logo" />
-    </div>
-  </header>
-);
+export const Header = () => {
+  //一変apiを二回呼び出すように見えるが、実はapolloが凄過ぎてuseMeのデータがcacheにあることをしている。
+  //なので二度apiを呼ばない。
+  const { data } = useMe();
+  return (
+    <header className="py-4">
+      <div className=" pl-3 pr-7 xl:px-0 mx-auto w-screen max-w-screen-2xl flex justify-between items-center bg-gray-200">
+        <img
+          src={higawariLogo}
+          className=" w-36 mb-5"
+          alt="Higawari-eats-logo"
+        />
+        <span className="text-xs">
+          <Link to="/my-profile">
+            <FontAwesomeIcon
+              icon={faUser}
+              className="text-2xl text-yellow-600 text-opacity-75"
+            />
+          </Link>
+        </span>
+      </div>
+    </header>
+  );
+};
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,4 +1,3 @@
-import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -6,10 +5,9 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
-import { isLoggedInVar } from "../apollo";
 import { Header } from "../components/header";
+import { useMe } from "../hooks/useMe";
 import { Restaurants } from "../pages/client/restaurants";
-import { meQuery } from "../__generated__/meQuery";
 
 //switchはchildとしてrouteしか持つ事ができない、だがClientRoutesはfragment <></>をreturnするからエラーになる。
 //直し方は簡単arrayし変える。
@@ -19,19 +17,8 @@ const ClientRoutes = [
   </Route>,
 ];
 
-const ME_QUERY = gql`
-  query meQuery {
-    me {
-      id
-      email
-      role
-      verified
-    }
-  }
-`;
-
 const LoggedInRouter = () => {
-  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  const { data, error, loading } = useMe();
   if (loading || error || !data) {
     return (
       <div className="h-screen flex justify-center items-center">
