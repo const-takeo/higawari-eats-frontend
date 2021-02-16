f84a79a43c1ded6ced16e8ae95ee5133dffa7876
const-takeo <const.takeo@gmail.com>
2021-02-17 03:30:17 +0900

    Restaurant Design

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import HigawariBack from "../../images/higawariback.jpg";
 import React from "react";
 import {
   restaurantPageQuery,
@@ -48,6 +49,37 @@ export const Restaurants = () => {
       },
     },
   });
-  console.log(data);
-  return <h1>Restaurants</h1>;
+  return (
+    <div className="relative w-full h-96 opacity-80">
+      <img
+        src={HigawariBack}
+        alt="higawari-eats-background"
+        className="w-full h-full"
+      />
+      <form className="absolute top-1/2 left-1/3 w-4/12">
+        <input
+          className="inputCss w-full border-0 items-center justify-center"
+          type="Search"
+          placeholder="探してるレストランは？"
+        />
+      </form>
+      {!loading && (
+        <div className="mt-5 mx-auto max-w-screen-2xl">
+          <div className="flex justify-around max-w-xs mx-auto">
+            {data?.allCategories.categories?.map((category) => (
+              <div className="flex flex-col items-center cursor-pointer rounded-2xl hover:bg-gray-200">
+                <div
+                  className="w-14 h-14 rounded-full bg-yellow-500 bg-cover"
+                  style={{ backgroundImage: `url(${category.coverImg})` }}
+                ></div>
+                <span className="mt-3 text-sm text-center font-medium">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
