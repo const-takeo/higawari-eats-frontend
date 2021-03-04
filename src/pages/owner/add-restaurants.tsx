83f8ae98a46d494521041116ea1a213f3588a07d
const-takeo <const.takeo@gmail.com>
2021-03-05 01:29:34 +0900

    Execute Mutation

--- src/pages/owner/add-restaurants.tsx
+++ src/pages/owner/add-restaurants.tsx
@@ -1,8 +1,9 @@
 import { gql, useMutation } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Button } from "../../components/button";
+import { FormError } from "../../components/form-error";
 import {
   createRestaurants,
   createRestaurantsVariables,
@@ -22,9 +23,23 @@ interface IFormProps {
   name: string;
   address: string;
   categoryName: string;
+  file: FileList;
 }
 
 export const AddRestaurant = () => {
+  const onCompleted = (data: createRestaurants) => {
+    const {
+      createRestaurants: { ok, restaurantId, error },
+    } = data;
+    console.log(error);
+    if (ok) {
+      setUploading(false);
+    }
+  };
+  const [createRestaurantMutation, { data }] = useMutation<
+    createRestaurants,
+    createRestaurantsVariables
+  >(CREATE_RESTAURANT_MUTATION, { onCompleted });
   const {
     register,
     handleSubmit,
@@ -32,47 +47,85 @@ export const AddRestaurant = () => {
     formState,
     errors,
   } = useForm<IFormProps>({ mode: "onChange" });
-  const [createRestaurantMutation, { loading, data }] = useMutation<
-    createRestaurants,
-    createRestaurantsVariables
-  >(CREATE_RESTAURANT_MUTATION);
-  const onSubmit = () => {
-    console.log(getValues());
+  const [uploading, setUploading] = useState(false);
+  const onSubmit = async () => {
+    try {
+      setUploading(true);
+      const { file, name, categoryName, address } = getValues();
+      const actualFile = file[0];
+      const formBody = new FormData();
+      formBody.append("file", actualFile);
+      console.log(name, address);
+      //request
+      const { url: coverImg } = await (
+        await fetch("http://localhost:4000/uploads/", {
+          method: "POST",
+          body: formBody,
+        })
+      ).json();
+      createRestaurantMutation({
+        variables: {
+          input: {
+            name,
+            categoryName,
+            address,
+            coverImg,
+          },
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     //   input nameはpropsのfield名と一緒にする事
-    <div className="commonContainer">
+    <div className="commonContainer w-full max-w-screen-sm flex flex-col items-center px-5">
       <Helmet>
         <title> Create Restaurant | Higawari Eats</title>
       </Helmet>
-      <h1>addRestaurant</h1>
-      <form onSubmit={onSubmit}>
+      <h4 className="font-semibold text-2xl mb-3">レストラン作成</h4>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid gap-3 mt-5 w-full mb-4"
+      >
         <input
           className="inputCss"
           type="text"
           name="name"
-          placeholder="Name"
-          ref={register({ required: "入力が必要です。" })}
+          placeholder="商名"
+          ref={register({ required: "入力が必要です。", minLength: 4 })}
         />
+        {errors.name?.type === "minLength" && (
+          <FormError errorMessage={"4文字以上入力してください。"} />
+        )}
         <input
           className="inputCss"
           type="text"
           name="address"
-          placeholder="Address"
+          placeholder="住所"
           ref={register({ required: "入力が必要です。" })}
         />
         <input
           className="inputCss"
           type="text"
           name="categoryName"
-          placeholder="Category Name"
+          placeholder="カテゴリー名"
           ref={register({ required: "入力が必要です。" })}
         />
+        <input
+          type="file"
+          name="file"
+          accept="image/*"
+          ref={register({ required: true })}
+        />
         <Button
-          loading={loading}
+          loading={uploading}
           canClick={formState.isValid}
           actionText="作成"
         />
+        {data?.createRestaurants?.error && (
+          <FormError errorMessage={data.createRestaurants.error} />
+        )}
       </form>
     </div>
   );
