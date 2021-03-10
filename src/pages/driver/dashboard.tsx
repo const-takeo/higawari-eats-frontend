7b24dd8a53fba42be3d2937df3a2fe7ac9eafaf3
const-takeo <const.takeo@gmail.com>
2021-03-11 00:27:15 +0900

    GeoLocation

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,6 +1,32 @@
 import GoogleMapReact from "google-map-react";
+import { useEffect, useState } from "react";
+
+interface ICoords {
+  lat: number;
+  lng: number;
+}
 
 export const DashBoard = () => {
+  const [driverCoords, setDriverCoords] = useState<ICoords>({ lat: 0, lng: 0 });
+  const onSuccess = (position: GeolocationPosition) => {
+    const {
+      coords: { latitude, longitude },
+    } = position;
+    setDriverCoords({ lat: latitude, lng: longitude });
+    console.log(latitude, longitude);
+  };
+  const onError = (error: GeolocationPositionError) => {
+    console.log(error);
+  };
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSuccess, onError, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+  const onAPiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    console.log(map, maps);
+    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+  };
   return (
     <div>
       <div
@@ -8,12 +34,12 @@ export const DashBoard = () => {
         className="overflow-hidden"
       >
         <GoogleMapReact
-          defaultZoom={20}
-          defaultCenter={{ lat: 59.95, lng: 30.33 }}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={onAPiLoaded}
+          defaultZoom={15}
+          defaultCenter={{ lat: 35.7594578, lng: 139.67185949999998 }}
           bootstrapURLKeys={{ key: "AIzaSyCmkLHf7k00Rt8rgI8SzCVpg2buOPKaDvI" }}
-        >
-          <h1>Hello</h1>
-        </GoogleMapReact>
+        ></GoogleMapReact>
       </div>
     </div>
   );
