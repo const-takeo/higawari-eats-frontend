cb0b9460628e8aa0f602ecb2414c0300d2cba5ea
const-takeo <const.takeo@gmail.com>
2021-03-11 18:00:12 +0900

    Geocoding

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -6,9 +6,17 @@ interface ICoords {
   lng: number;
 }
 
+interface IDriverProps {
+  lat: number;
+  lng: number;
+  $hover?: any;
+}
+
+const Driver: React.FC<IDriverProps> = () => <div className="text-lg">🚘</div>;
+
 export const DashBoard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lat: 0, lng: 0 });
-  const [map, setMap] = useState<any>();
+  const [map, setMap] = useState<google.maps.Map>();
   const [maps, setMaps] = useState<any>();
   const onSuccess = (position: GeolocationPosition) => {
     const {
@@ -20,6 +28,11 @@ export const DashBoard = () => {
   const onError = (error: GeolocationPositionError) => {
     console.log(error);
   };
+  const onAPiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
+  };
   useEffect(() => {
     navigator.geolocation.watchPosition(onSuccess, onError, {
       enableHighAccuracy: true,
@@ -27,14 +40,20 @@ export const DashBoard = () => {
   }, []);
   useEffect(() => {
     if (map && maps) {
-      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+      map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+      //geocoding
+      //   const geocoder = new google.maps.Geocoder();
+      //   geocoder.geocode(
+      //     {
+      //       location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+      //     },
+      //     (results, status) => {
+      //       console.log(results);
+      //       console.log(status);
+      //     }
+      //   );
     }
-  }, [driverCoords.lat, driverCoords.lng]);
-  const onAPiLoaded = ({ map, maps }: { map: any; maps: any }) => {
-    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
-    setMap(map);
-    setMaps(maps);
-  };
+  }, [driverCoords.lat, driverCoords.lng, map, maps]);
   return (
     <div>
       <div
@@ -48,14 +67,7 @@ export const DashBoard = () => {
           defaultCenter={{ lat: 35.7594578, lng: 139.67185949999998 }}
           bootstrapURLKeys={{ key: "AIzaSyCmkLHf7k00Rt8rgI8SzCVpg2buOPKaDvI" }}
         >
-          <div
-            className="text-lg"
-            //@ts-ignore
-            lat={driverCoords.lat}
-            lng={driverCoords.lng}
-          >
-            🚘
-          </div>
+          <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
         </GoogleMapReact>
       </div>
     </div>
